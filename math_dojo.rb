00ef405eab4b2d1bf50596b0c4cba88d780c08b8
Mike Dacanay <migd64@gmail.com>
2017-10-03 12:47:54 -0700

    final commit

--- math_dojo.rb
+++ math_dojo.rb
@@ -1,14 +1,20 @@
 class MathDojo
 	@@counter = 0
 	def add(num1 = 0,num2 = 0)
-		@@counter= num1+num2
-		puts @@counter
-		# self
+		@@counter= @@counter+num1+num2
+		# puts @@counter
+		self
 	end
 
-	# def add(num1 = 0, num2 = 0)
-	# 	puts num
-	# end
+	def subtract(num1 = 0, num2 = 0)
+		@@counter= @@counter-num1-num2
+		# puts @@counter
+		self
+	end
+
+	def result
+		puts @@counter
+	end
 end
 
-math1 = MathDojo.new.add(1,3)
+math1 = MathDojo.new.add(1,3).subtract(1,3).result
